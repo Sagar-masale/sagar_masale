1c3e139885f3773effd2324bf6323345f159f006
sagar-masale <sagarmasale27@gmail.com>
2025-06-19 20:38:31 +0530

    fix Contact bug

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -24,7 +24,7 @@ const handleSubmit = async (e: React.FormEvent) => {
   setLoading(true);
 
   try {
-    const res = await fetch(`${process.env.Backend_Url}/api/send`, {
+    const res = await fetch(`${import.meta.env.Backend_Url}/api/send`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
